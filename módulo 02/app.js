69150d31ac36d3de60205756187fef477f1bc40d
eramoscar <eramoscar@uoc.edu>
2020-06-03 22:03:11 +0200

    module 2 almost

--- módulo 02/app.js
+++ módulo 02/app.js
@@ -1,57 +1,52 @@
-// var hotel = {
-//     name: "Mencey",
-//     location: "Tenerife",
-//     img: "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/2f/e9/d7/iberostar-grand-mencey.jpg",
-// };
-
-// document.getElementById("name-hotel").innerHTML = "Hotel " + hotel.name;
-// document.getElementById("location-hotel").innerHTML =
-//     "Ubicado en " + hotel.location;
-// document.getElementById("img-hotel").src = hotel.img;
-
-// var rating = prompt("Puntuación: del 1 al 5");
-
-// document.getElementById("rating").innerHTML = rating + " estrellas";
-
-// var anonymous = confirm("¿Quiere que la reseña sea anónima?");
-
-// document.getElementById("anonymous").checked = anonymous;
-
-// __________________________________________________________________
-
-
 var hoteles = {
-    Mencey: {
-        name: "Mencey",
-        location: "Tenerife",
-        img: "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/2f/e9/d7/iberostar-grand-mencey.jpg",
+    WBarcelona: {
+        name: "W Barcelona",
+        location: "Barcelona",
+        img: "https://www.7canibales.com/wp-content/uploads/2009/10/w-barcelona.jpg",
     },
-    Lani: {
-        name: "Lani's Suites Deluxe",
-        location: "Lanzarote",
-        img: "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/49/47/e3/lani-s-suites-deluxe.jpg",
+    Arts: {
+        name: "Arts",
+        location: "Barcelona",
+        img: "https://info.bcnluxapartments.com/wp-content/uploads/2018/06/Hotel_Arts_A_exterior_01-221x300.jpg",
     },
 };
 
 
-var selectedHotel = prompt("Indique hotel sobre el que quiere hacer la reseña: Mencey o Lani");
+var selectedHotel = prompt("Indique hotel sobre el que quiere hacer la reseña: WBarcelona o Arts");
 
+document.getElementById("name-hotel").innerHTML = "Hotel " + hoteles[selectedHotel].name + "<hr>";
 
-document.getElementById("name-hotel").innerHTML = "Hotel " + hoteles[selectedHotel].name;
-
-document.getElementById("location-hotel").innerHTML = "Ubicado en " + hoteles[selectedHotel].location;
+document.getElementById("location-hotel").innerHTML = "Ubicado en " + hoteles[selectedHotel].location + "<hr>";
 
 document.getElementById("img-hotel").src = hoteles[selectedHotel].img;
 
+var anonymous = confirm("¿Quiere que la reseña sea anónima?");
+
+document.getElementById("anonymous").checked = anonymous;
+
 
 var stars = {
+    bajo: "<span>&#0128;</span>",
+
     una: "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+
     dos: "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+
+    medio: "<span>&#0128;</span><span>&#0128;</span><span>&#0128;</span>",
+
     tres: "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span>",
+
     cuatro: "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span>",
+
+    alto: "<span>&#0128;</span><span>&#0128;</span><span>&#0128;</span><span>&#0128;</span><span>&#0128;</span>",
+
     cinco: "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>",
 };
 
 var rating = prompt("Puntuación: una, dos, tres, cuatro o cinco estrellas");
 
+var coste = prompt("Coste: alto, medio o bajo");
+
 document.getElementById("rating").innerHTML = stars[rating];
+
+document.getElementById("coste").innerHTML = stars[coste];
