83e3f9f7b8132e2207cfad147f0f165f464ee6c3
eramoscar <eramoscar@uoc.edu>
2020-07-01 21:23:36 +0200

    update

--- módulo 04/mod04.js
+++ módulo 04/mod04.js
@@ -9,19 +9,34 @@ var resta = (a, b) => a - b;
 var multiplicacion = (a, b) => a * b;
 var division = (a, b) => a / b;
 
-console.log(suma(5, 5));
-console.log(resta(5, 5));
-console.log(multiplicacion(5, 5));
-console.log(division(5, 5));
+// Inputs
+var num1 = () => parseInt(document.getElementById("campo1").value);
+var num2 = () => parseInt(document.getElementById("campo2").value);
 
-function calc() {
-    // obtener primer numero
-    var num1 = document.getElementById("campo1").value;
+var SumResult = document.getElementById("resultado").innerText = suma(num1(), num2());
+var SubsResult = document.getElementById("resultado").innerText = resta(num1(), num2());
+var MultResult = document.getElementById("resultado").innerText = multiplicacion(num1(), num2());
+var DivResult = document.getElementById("resultado").innerText = division(num1(), num2());
 
-    // obtener segundo numero
-    var num2 = document.getElementById("campo2").value;
+document.getElementById("button-mas") - addEventListener("click", SumResult);
+document.getElementById("button-menos") - addEventListener("click", SumResult);
+document.getElementById("button-por") - addEventListener("click", SumResult);
+document.getElementById("button-dividido") - addEventListener("click", SumResult);
 
-    // obtener resultado
-    var resul = document.getElementById("campo3").value;
-    resul =
-}
+
+
+
+// () => document.getElementById("campo3").value;
+
+// var sumar = () => {
+//     console.log("resultado: " + suma(num1(), num2()));
+// }
+
+
+//(suma(num1, num2)) => document.getElementById("button-mas").value;
+
+
+
+// var restar = (resta(num1, num2)) => document.getElementById("button-menos").value;
+// var multiplicar = (multiplicacion(num1, num2)) => document.getElementById("button-por").value;
+// var dividir = (division(num1, num2)) => document.getElementById("button-dividido").value;
