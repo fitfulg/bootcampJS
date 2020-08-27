763a85bc36a7ba9493db7f63972ced0612536228
Eric <rc84.eric@gmail.com>
2020-08-27 15:08:06 +0200

    no message

---
+++ Módulo 12 - Caso práctico - Banca Online/00-boilerplate/src/pages/account/account.mappers.js
@@ -0,0 +1,8 @@
+export const mapAccountVmToApi = account => ({
+    ...account,
+    name: account.alias,
+});
+export const mapAccountApiToVm = account => ({
+    ...account,
+    alias: account.name,
+});
