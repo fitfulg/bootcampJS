4a5acc6a120506ee5f45f29c7623ada171234644
Eric <rc84.eric@gmail.com>
2020-09-27 10:52:55 +0200

    property-list

--- Módulo 12 - Caso práctico - Portal inmobiliario/00-boilerplate/src/pages/property-list/property-list.helpers.js
+++ Módulo 12 - Caso práctico - Portal inmobiliario/00-boilerplate/src/pages/property-list/property-list.helpers.js
@@ -3,138 +3,138 @@ import phoneImage from '../../core/content/img/telefono_icon.svg';
 import { routes } from '../../core/router';
 
 const getImage = property => {
-  const image = document.createElement('img');
-  image.src = property.image;
+    const image = document.createElement('img');
+    image.src = property.image;
 
-  return image;
+    return image;
 };
 
 const getTitle = property => {
-  const title = document.createElement('h1');
-  title.textContent = property.title;
+    const title = document.createElement('h1');
+    title.textContent = property.title;
 
-  return title;
+    return title;
 };
 
 const getSubtitle = property => {
-  const subtitle = document.createElement('h2');
+    const subtitle = document.createElement('h2');
 
-  const rooms = document.createElement('span');
-  rooms.textContent = property.rooms;
+    const rooms = document.createElement('span');
+    rooms.textContent = property.rooms;
 
-  const squareMeter = document.createElement('span');
-  squareMeter.textContent = property.squareMeter;
+    const squareMeter = document.createElement('span');
+    squareMeter.textContent = property.squareMeter;
 
-  subtitle.appendChild(rooms);
-  subtitle.appendChild(squareMeter);
+    subtitle.appendChild(rooms);
+    subtitle.appendChild(squareMeter);
 
-  return subtitle;
+    return subtitle;
 };
 
 const getNotes = property => {
-  const notes = document.createElement('p');
-  notes.textContent = property.notes;
+    const notes = document.createElement('p');
+    notes.textContent = property.notes;
 
-  return notes;
+    return notes;
 };
 
 const getContactIcons = () => {
-  const list = document.createElement('ul');
+    const list = document.createElement('ul');
 
-  const email = document.createElement('li');
-  const emailIcon = document.createElement('img');
-  emailIcon.src = emailImage;
-  email.appendChild(emailIcon);
+    const email = document.createElement('li');
+    const emailIcon = document.createElement('img');
+    emailIcon.src = emailImage;
+    email.appendChild(emailIcon);
 
-  const phone = document.createElement('li');
-  const phoneIcon = document.createElement('img');
-  phoneIcon.src = phoneImage;
-  phone.appendChild(phoneIcon);
+    const phone = document.createElement('li');
+    const phoneIcon = document.createElement('img');
+    phoneIcon.src = phoneImage;
+    phone.appendChild(phoneIcon);
 
-  list.appendChild(email);
-  list.appendChild(phone);
+    list.appendChild(email);
+    list.appendChild(phone);
 
-  return list;
+    return list;
 };
 
 const getPrice = property => {
-  const container = document.createElement('div');
-  container.classList.add('contenedor-precio');
+    const container = document.createElement('div');
+    container.classList.add('contenedor-precio');
 
-  const price = document.createElement('div');
-  price.classList.add('precio');
-  price.textContent = property.price;
+    const price = document.createElement('div');
+    price.classList.add('precio');
+    price.textContent = property.price;
 
-  container.appendChild(price);
+    container.appendChild(price);
 
-  return container;
+    return container;
 };
 
 const getPropetyRow = property => {
-  const image = getImage(property);
+    const image = getImage(property);
 
-  const title = getTitle(property);
-  const subtitle = getSubtitle(property);
-  const notes = getNotes(property);
+    const title = getTitle(property);
+    const subtitle = getSubtitle(property);
+    const notes = getNotes(property);
 
-  const navBottom = document.createElement('div');
-  navBottom.classList.add('nav-bottom');
-  const contactIcons = getContactIcons();
-  const price = getPrice(property);
-  navBottom.appendChild(contactIcons);
-  navBottom.appendChild(price);
+    const navBottom = document.createElement('div');
+    navBottom.classList.add('nav-bottom');
+    const contactIcons = getContactIcons();
+    const price = getPrice(property);
+    navBottom.appendChild(contactIcons);
+    navBottom.appendChild(price);
 
-  const article = document.createElement('article');
+    const article = document.createElement('article');
 
-  const figure = document.createElement('figure');
-  figure.appendChild(image);
-  article.appendChild(figure);
+    const figure = document.createElement('figure');
+    figure.appendChild(image);
+    article.appendChild(figure);
 
-  const description = document.createElement('div');
-  article.appendChild(description);
-  description.appendChild(title);
-  description.appendChild(subtitle);
-  description.appendChild(notes);
-  description.appendChild(navBottom);
+    const description = document.createElement('div');
+    article.appendChild(description);
+    description.appendChild(title);
+    description.appendChild(subtitle);
+    description.appendChild(notes);
+    description.appendChild(navBottom);
 
-  const row = document.createElement('a');
-  row.href = routes.propertyDetail(property.id);
-  row.appendChild(article);
+    const row = document.createElement('a');
+    row.href = routes.propertyDetail(property.id);
+    row.appendChild(article);
 
-  return row;
+    return row;
 };
 
 export const addPropertyRows = propertyList => {
-  propertyList.forEach(property => {
-    const row = getPropetyRow(property);
-    const listElement = document.getElementById('property-list');
-    listElement.appendChild(row);
-  });
+    propertyList.forEach(property => {
+        const row = getPropetyRow(property);
+        const listElement = document.getElementById('property-list');
+        listElement.appendChild(row);
+    });
 };
 
 export const clearPropertyRows = () => {
-  const listElement = document.getElementById('property-list');
-  listElement.innerHTML = '';
+    const listElement = document.getElementById('property-list');
+    listElement.innerHTML = '';
 };
 
 const getOption = item => {
-  const option = document.createElement('option');
-  option.value = item.id;
-  option.textContent = item.name;
+    const option = document.createElement('option');
+    option.value = item.id;
+    option.textContent = item.name;
 
-  return option;
+    return option;
 };
 
 export const setOptions = (list, id, defaultValue) => {
-  const select = document.getElementById(id);
+    const select = document.getElementById(id);
 
-  const defaultOption = document.createElement('option');
-  defaultOption.value = '';
-  defaultOption.textContent = defaultValue;
-  select.appendChild(defaultOption);
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.textContent = defaultValue;
+    select.appendChild(defaultOption);
 
-  list.forEach(item => {
-    const option = getOption(item);
-    select.appendChild(option);
-  });
+    list.forEach(item => {
+        const option = getOption(item);
+        select.appendChild(option);
+    });
 };
